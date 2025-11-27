680957aca11492fd9f0e3bcba099ce81e949fc73
vaishnavirai2075 <vaishnavi24599@iiitd.ac.in>
2025-11-27 18:36:42 +0530

    updated MaintenanceDao.java

--- src/main/java/edu/univ/erp/data/MaintenanceDao.java
+++ src/main/java/edu/univ/erp/data/MaintenanceDao.java
@@ -1,7 +1,5 @@
 package edu.univ.erp.data;
 
-import edu.univ.erp.util.DataSourceProvider;
-
 import javax.sql.DataSource;
 import java.sql.*;
 
@@ -12,31 +10,22 @@ public class MaintenanceDao {
         this.ds = ds;
     }
 
-   //check is maintenance is on
+    // checks if the system is currently in maintenance mode 
+    // returns true if it is on
     public boolean isMaintenanceOn() throws SQLException {
-        String sql = "SELECT setting_value FROM settings WHERE setting_key = 'maintenance_mode'";
+        var sql = "SELECT setting_value FROM settings WHERE setting_key = 'maintenance_mode'";
 
-        try (Connection conn = ds.getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql);
-             ResultSet rs = ps.executeQuery()) {
-
-            if (!rs.next()) {
-                return false; // default OFF
-            }
-
-            return "on".equalsIgnoreCase(rs.getString(1));
+        try (var conn = ds.getConnection(); var ps = conn.prepareStatement(sql); var rs = ps.executeQuery()) {
+            // Returns true only if a setting exists and it is set to "on"
+            return rs.next() && "on".equalsIgnoreCase(rs.getString(1));
         }
     }
 
-    //change maintenance flag
+    // turns the maintenance mode on or off by saving the setting to the database
     public void setMaintenance(boolean on) throws SQLException {
-        String sql =
-                "INSERT INTO settings (setting_key, setting_value) " +
-                        "VALUES ('maintenance_mode', ?) " +
-                        "ON DUPLICATE KEY UPDATE setting_value = VALUES(setting_value)";
+        var sql = "INSERT INTO settings (setting_key, setting_value) VALUES ('maintenance_mode', ?) ON DUPLICATE KEY UPDATE setting_value = VALUES(setting_value)";
 
-        try (Connection conn = ds.getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql)) {
+        try (var conn = ds.getConnection(); var ps = conn.prepareStatement(sql)) {
             ps.setString(1, on ? "on" : "off");
             ps.executeUpdate();
         }
