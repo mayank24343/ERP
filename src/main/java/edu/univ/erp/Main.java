f9e8547f2e87def25274940af3afc480db1455b9
mayank24343 <mayank24343@iiitd.ac.in>
2025-11-27 13:22:16 +0530

    added main for final running

--- src/main/java/edu/univ/erp/Main.java
+++ src/main/java/edu/univ/erp/Main.java
@@ -1,7 +1,20 @@
 package edu.univ.erp;
 
-public class Main {
-    public static void main(String[] args) {
+import edu.univ.erp.ui.LoginWindow;
+import edu.univ.erp.ui.UiContext;
 
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Swing UI must run on EDT
+        SwingUtilities.invokeLater(() -> {
+            UiContext ctx = UiContext.get();
+
+            LoginWindow login = new LoginWindow(ctx.auth(), ctx.users());
+            login.setVisible(true);
+        });
     }
 }
+
