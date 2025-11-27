10435f8a4fd9a0379c2a9f256e55c174c210f913
vaishnavirai2075 <vaishnavi24599@iiitd.ac.in>
2025-11-27 18:34:38 +0530

    updated InstructorDao.java

--- src/main/java/edu/univ/erp/data/InstructorDao.java
+++ src/main/java/edu/univ/erp/data/InstructorDao.java
@@ -1,12 +1,8 @@
 package edu.univ.erp.data;
 
-import edu.univ.erp.domain.Course;
 import edu.univ.erp.domain.Instructor;
-
 import javax.sql.DataSource;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class InstructorDao {
     private final DataSource ds;
@@ -15,17 +11,11 @@ public class InstructorDao {
         this.ds = ds;
     }
 
-    public void insertInstructor(String userId, String department, String designation)
-            throws SQLException {
+    // it saves a new instructor's specific details (department + job title) to the database
+    public void insertInstructor(String userId, String department, String designation) throws SQLException {
+        var sql = "INSERT INTO instructors (user_id, department, designation) VALUES (?, ?, ?)";
 
-        String sql = """
-            INSERT INTO instructors (user_id, department, designation)
-            VALUES (?, ?, ?)
-        """;
-
-        try (Connection c = ds.getConnection();
-             PreparedStatement ps = c.prepareStatement(sql)) {
-
+        try (var c = ds.getConnection(); var ps = c.prepareStatement(sql)) {
             ps.setString(1, userId);
             ps.setString(2, department);
             ps.setString(3, designation);
