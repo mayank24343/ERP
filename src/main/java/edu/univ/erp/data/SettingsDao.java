36eaf11b392e6980fe6bd989cf2523dd7b2852b3
vaishnavirai2075 <vaishnavi24599@iiitd.ac.in>
2025-11-27 18:50:30 +0530

    updated SettingsDao.java

--- src/main/java/edu/univ/erp/data/SettingsDao.java
+++ src/main/java/edu/univ/erp/data/SettingsDao.java
@@ -11,28 +11,22 @@ public class SettingsDao {
         this.ds = ds;
     }
 
+    // checks the database to see if maintenance mode is currently enabled
+    //looks for true
     public boolean readMaintenanceFlag() throws SQLException {
-        String sql = "SELECT setting_value FROM settings WHERE setting_key = 'maintenance_mode'";
+        var sql = "SELECT setting_value FROM settings WHERE setting_key = 'maintenance_mode'";
 
-        try (Connection c = ds.getConnection();
-             PreparedStatement ps = c.prepareStatement(sql)) {
-
-            ResultSet rs = ps.executeQuery();
-            if (!rs.next()) return false;
-
-            return rs.getString("setting_value").equalsIgnoreCase("true");
+        try (var c = ds.getConnection(); var ps = c.prepareStatement(sql); var rs = ps.executeQuery()) {
+            // Return true only if the row exists and the value is "true"
+            return rs.next() && "true".equalsIgnoreCase(rs.getString("setting_value"));
         }
     }
 
+    // updates the maintenance mode setting in the database (true / false)
     public void writeMaintenanceFlag(boolean enabled) throws SQLException {
-        String sql = """
-            UPDATE settings SET setting_value = ?
-            WHERE setting_key = 'maintenance_mode'
-        """;
+        var sql = "UPDATE settings SET setting_value = ? WHERE setting_key = 'maintenance_mode'";
 
-        try (Connection c = ds.getConnection();
-             PreparedStatement ps = c.prepareStatement(sql)) {
-
+        try (var c = ds.getConnection(); var ps = c.prepareStatement(sql)) {
             ps.setString(1, enabled ? "true" : "false");
             ps.executeUpdate();
         }
