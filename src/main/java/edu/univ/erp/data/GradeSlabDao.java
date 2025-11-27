6ff420dbdb69893279c62465469d27a0713bd1e7
vaishnavirai2075 <vaishnavi24599@iiitd.ac.in>
2025-11-27 18:32:23 +0530

    update GradeSlabDao.java

--- src/main/java/edu/univ/erp/data/GradeSlabDao.java
+++ src/main/java/edu/univ/erp/data/GradeSlabDao.java
@@ -1,7 +1,6 @@
 package edu.univ.erp.data;
 
 import edu.univ.erp.domain.GradeSlab;
-
 import javax.sql.DataSource;
 import java.sql.*;
 import java.util.*;
@@ -9,68 +8,64 @@ import java.util.*;
 public class GradeSlabDao {
     private final DataSource ds;
 
-    //constructor
     public GradeSlabDao(DataSource ds) {
         this.ds = ds;
     }
 
-    //get slabs
+    // gets all grade ranges or slabs for a specific section
+    // the order is from highest to lowest
     public List<GradeSlab> getSlabs(int sectionId) throws SQLException {
-        String sql = "SELECT * FROM grade_slabs WHERE section_id=? ORDER BY min_percent DESC";
-        List<GradeSlab> list = new ArrayList<>();
+        var sql = "SELECT * FROM grade_slabs WHERE section_id=? ORDER BY min_percent DESC";
+        var list = new ArrayList<GradeSlab>();
 
-        try (Connection conn = ds.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
-
+        try (var conn = ds.getConnection(); var ps = conn.prepareStatement(sql)) {
             ps.setInt(1, sectionId);
-            ResultSet rs = ps.executeQuery();
-
-            while (rs.next()) {
-                list.add(new GradeSlab(
+            try (var rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    list.add(new GradeSlab(
                         rs.getInt("slab_id"),
                         rs.getInt("section_id"),
                         rs.getString("letter"),
                         rs.getDouble("min_percent"),
                         rs.getDouble("max_percent")
-                ));
+                    ));
+                }
             }
         }
-
         return list;
     }
 
-    //add slabs
+    // adds a new grade range definition (like 'A' is 90-100)
     public void insertSlab(int sectionId, String letter, double min, double max) throws SQLException {
-        String sql = "INSERT INTO grade_slabs (section_id, letter, min_percent, max_percent) VALUES (?, ?, ?, ?)";
+        var sql = "INSERT INTO grade_slabs (section_id, letter, min_percent, max_percent) VALUES (?, ?, ?, ?)";
 
-        try (Connection conn = ds.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
-
+        try (var conn = ds.getConnection(); var ps = conn.prepareStatement(sql)) {
             ps.setInt(1, sectionId);
             ps.setString(2, letter);
             ps.setDouble(3, min);
             ps.setDouble(4, max);
-
             ps.executeUpdate();
         }
     }
 
-    //updet slabs
+    // updates an existing grade range definition
     public void updateSlab(GradeSlab slab) throws SQLException {
-        String sql = "UPDATE grade_slabs SET letter=?, min_percent=?, max_percent=? WHERE slab_id=?";
+        var sql = "UPDATE grade_slabs SET letter=?, min_percent=?, max_percent=? WHERE slab_id=?";
 
-        try (Connection conn = ds.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
-
+        try (var conn = ds.getConnection(); var ps = conn.prepareStatement(sql)) {
             ps.setString(1, slab.getLetter());
             ps.setDouble(2, slab.getMin());
             ps.setDouble(3, slab.getMax());
             ps.setInt(4, slab.getId());
-
             ps.executeUpdate();
         }
     }
 
-    //delete slabs
+    // deletes a grade range
     public void deleteSlab(int slabId) throws SQLException {
-        try (Connection conn = ds.getConnection(); PreparedStatement ps = conn.prepareStatement("DELETE FROM grade_slabs WHERE slab_id=?")) {
+        var sql = "DELETE FROM grade_slabs WHERE slab_id=?";
+
+        try (var conn = ds.getConnection(); var ps = conn.prepareStatement(sql)) {
             ps.setInt(1, slabId);
             ps.executeUpdate();
         }
